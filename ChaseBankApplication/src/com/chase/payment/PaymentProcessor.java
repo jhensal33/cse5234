acb87c39e5032c23aefc181d53a0335b2166b0b7
Jeff Hensal <hensal.7@osu.edu>
2019-10-30 18:34:29 -0400

    Adding chase bank application

---
+++ ChaseBankApplication/src/com/chase/payment/PaymentProcessor.java
@@ -0,0 +1,16 @@
+package com.chase.payment;
+
+public class PaymentProcessor {
+
+	public String ping() {
+		return "Bank's Open";
+	}
+	
+	public String processPayment(CreditCardPayment payment) {
+		
+		String confNumber = "123";
+		
+		return confNumber;
+	}
+	
+}
