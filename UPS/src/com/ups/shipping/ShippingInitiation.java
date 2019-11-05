467bbc3402e53f5be541869d2eadb65bcc96ee42
Michael Lynch <lynchm09@gmail.com>
2019-11-04 19:25:11 -0500

    initiation method and web.xml modified

--- UPS/src/com/ups/shipping/ShippingInitiation.java
+++ UPS/src/com/ups/shipping/ShippingInitiation.java
@@ -1,8 +1,16 @@
 package com.ups.shipping;
 
 import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/initiation")
 public class ShippingInitiation {
+	
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public Response ping() {
+		return Response.ok("Hello, UPS is up and running to serve you ...").build();
+	}
 
 }
