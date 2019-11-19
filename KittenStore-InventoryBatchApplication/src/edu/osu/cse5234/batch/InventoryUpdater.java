0178b98ee22a06aea84a2cb270d60ccb589339d7
Michael Lynch <lynchm09@gmail.com>
2019-11-18 19:21:11 -0500

    Started implementing functions

--- KittenStore-InventoryBatchApplication/src/edu/osu/cse5234/batch/InventoryUpdater.java
+++ KittenStore-InventoryBatchApplication/src/edu/osu/cse5234/batch/InventoryUpdater.java
@@ -6,6 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.Map;
 
 public class InventoryUpdater {
@@ -27,7 +28,7 @@ public class InventoryUpdater {
 
 	private static Connection createConnection() throws SQLException, ClassNotFoundException {
 		Class.forName("org.h2.Driver");
-		Connection conn = DriverManager.getConnection("jdbc:h2:~/test", "sa", "");
+		Connection conn = DriverManager.getConnection("jdbc:h2:~/test", "sa", "password");
 		return conn;
 	}
 
@@ -46,8 +47,12 @@ public class InventoryUpdater {
 		// TODO Auto-generated method stub
 		// This method returns a map of two integers. The first Integer is item ID, and 
                  // the second is cumulative requested quantity across all new orders
-		return null;
+		Map<Integer, Integer> orderedLineItems = new HashMap<Integer, Integer>();
+		
+		return orderedLineItems;
 	}
+	
+	
 
 	private static void udpateInventory(Map<Integer, Integer> orderedItems, 
                 Connection conn) throws SQLException {
